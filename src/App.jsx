5a1c367ed45e45f79d811c54497104d8d9c1cf5a
Rabin Shah <108661077+rabin9087@users.noreply.github.com>
2024-02-09 01:10:04 +1100

    app.js file change

--- src/App.jsx
+++ src/App.jsx
@@ -37,9 +37,9 @@ function App() {
         <Route
           path="/admin-signup"
           element={
-            // <PrivateRouter>
+            <PrivateRouter>
               <SignUp />
-            // </PrivateRouter>
+            </PrivateRouter>
           }
         ></Route>
         <Route
